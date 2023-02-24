23c23093237ab9c634625c61d07540e4937d5d33
shb7815 <shb7815@gmail.com>
2023-02-24 15:52:54 +0900

    jQuery

--- ch07/src/main/webapp/jquery/jquery-simple.js
+++ ch07/src/main/webapp/jquery/jquery-simple.js
@@ -1,8 +1,38 @@
-var _jQuery = function() {
+var _jQuery = function(elements) {
 	
+	this.lengh = elements.length;
+	for(var i = 0; i < this.lengh; i++) {
+		this[i] = elements[i];
+	}
 }
 
+_jQuery.prototype.css = function(name, value) {
+	for(var i = 0; i < this.lengh; i++) {
+		this[i].style[name] = value;
+	}
+}
+
+_jQuery.prototype.click = function(handler) {
+	for(var i = 0; i < this.lengh; i++) {
+		this[i].addEventListener('click', handler);
+	}
+}
+
+
 var jQuery = function(param) {
+	
+	if(typeof(param) === 'function'){
+		window.addEventListener('load', param);
+	}
+	
+	var elements;
+	
+	if(typeof(param) === 'string'){
+		elements = document.querySelectorAll(param);
+	}
+	
+	return new _jQuery(elements || []);
+	
 	
 }
 
