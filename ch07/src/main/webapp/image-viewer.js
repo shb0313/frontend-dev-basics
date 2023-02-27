6994453bdc5675c4b4f566b0aab0658162616b8e
shb7815 <shb7815@gmail.com>
2023-02-27 19:48:17 +0900

    jQuery

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -1,43 +1,76 @@
+var slide = 0;
+var indexTmp = 0;
+
 imageViewer = {
+
 	init: function() {
-		for(var i = 0; i < 100; i++) {
-			this._changeImage();
-		}
+		$(imageViewer._init);
 	},
-	
+
+	_init: function() {
+		$('.image-viewer .buttons #btn-change').click(imageViewer._change);
+		$('.image-viewer .buttons #btn-slideshow').click(imageViewer._slide);
+	},
+
+	_slide: function() {
+		slide++;
+		var Interval = setInterval(function() {	
+			if (slide%2 == 1) {
+				console.log("start 슬라이드: " + slide);
+				imageViewer._changeImage();
+			} else {
+				console.log("stop 슬라이드: " + slide);
+				clearInterval(Interval);
+			}
+		}, 2000);
+	},
+
+	_change: function() {
+		console.log("chageClick");
+		imageViewer._changeImage();
+	},
+
 	_changeImage: function() {
-		var index = Math.floor(Math.random()*this._images.length);
-		console.log(index);
+		var index = Math.floor(Math.random() * this._images.length);
+		console.log("이미지 바꾸기");
+		
+		if(index == indexTmp) {
+			console.log("같은거");
+			imageViewer._changeImage();
+			return;
+		}
+		console.log(imageViewer._images[index].file);
+
+		$('div img').attr({
+			src: "images/" + imageViewer._images[index].file,
+			title: imageViewer._images[index].name,
+		});
+		indexTmp = index;
 	},
-	
+
 	_images: [{
-		name: '국화', 
+		name: '국화',
 		file: 'Chrysanthemum.jpg'
 	}, {
-		name: '사막', 
+		name: '사막',
 		file: 'Desert.jpg'
 	}, {
-		name: '수국', 
+		name: '수국',
 		file: 'Hydrangeas.jpg'
 	}, {
-		name: '해파리', 
+		name: '해파리',
 		file: 'Jellyfish.jpg'
 	}, {
-		name: '코알라', 
+		name: '코알라',
 		file: 'Koala.jpg'
 	}, {
-		name: '등대', 
+		name: '등대',
 		file: 'Lighthouse.jpg'
 	}, {
-		name: '팽귄', 
+		name: '팽귄',
 		file: 'Penguins.jpg'
 	}, {
-		name: '튤립', 
+		name: '튤립',
 		file: 'Tulips.jpg'
 	}]
-	
-	
-	
-	
-	
 };
