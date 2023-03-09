6ede3ed8967cd6e71b335e972cadb76d3afcdaa0
shb7815 <shb7815@gmail.com>
2023-03-09 13:13:50 +0900

    Parenthesis-less function & Template Literal

---
+++ javascript-practice/es6/ex09.js
@@ -0,0 +1,19 @@
+/*
+    Parenthesis-less function & Template Literal
+ */
+
+const div = function(s, ...v){
+    //console.log(typeof(s), s instanceof Array, s.length);
+    console.log(s, v);
+
+    return function(){
+        return '<div></div>';
+    }
+}
+
+const StyledDiv = div
+`
+    color: ${(props)=>{props.color ? props.color : '#111'}};
+    font-size: 20px;
+    font-weight: bold;
+`;
